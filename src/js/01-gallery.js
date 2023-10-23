19dd8ae27248b1699a7431092be7a2d15ea995d2
Nadya <timrova.nadiia1622@vu.cdu.edu.ua>
2023-10-23 18:17:05 +0300

    hw 8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,38 @@
-// Add imports above this line
+
 import { galleryItems } from './gallery-items';
+
+
+console.log(galleryItems);
 import SimpleLightbox from "simplelightbox";
 
 import "simplelightbox/dist/simple-lightbox.min.css";
-// Change code below this line
 
-console.log(galleryItems);
+
+const listImages = document.querySelector(".gallery");
+console.log(listImages);
+
+const imageMarkup = createImagesListMarkup(galleryItems);
+
+listImages.insertAdjacentHTML("beforeend", imageMarkup);
+
+
+
+function createImagesListMarkup(images) {
+  return images.map(({ preview, original, description }) => {
+    return `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img 
+      class="gallery__image" 
+      src="${preview}" 
+      alt="${description}" 
+      />
+   </a>
+</li>`;
+  }).join("");
+};
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+        captionsData: 'alt',
+        captionDelay: 250,
+      });
