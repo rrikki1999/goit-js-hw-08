8e0216bb5bd3baec3640bb9c3de1dbfc46724716
Nadya <timrova.nadiia1622@vu.cdu.edu.ua>
2023-10-28 17:06:23 +0300

    hw 8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
 
+import throttle from 'lodash.throttle';
+
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.querySelector('.feedback-form');
+  const emailInput = form.querySelector('input[name="email"]');
+  const messageInput = form.querySelector('textarea[name="message"]');
+  const localStorageKey = 'feedback-form-state';
+
+  const saveStateToLocalStorage = throttle(function () {
+    const feedbackState = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+    localStorage.setItem(localStorageKey, JSON.stringify(feedbackState));
+  }, 500);
+
+  const savedState = JSON.parse(localStorage.getItem(localStorageKey));
+
+  if (savedState) {
+    emailInput.value = savedState.email;
+    messageInput.value = savedState.message;
+  }
+
+  form.addEventListener('input', saveStateToLocalStorage);
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const formData = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+
+    console.log(formData);
+
+    localStorage.removeItem(localStorageKey);
+
+    emailInput.value = '';
+    messageInput.value = '';
+  });
+});
